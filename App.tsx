9933bf1fd0940cc6e1ac40076912f2ca7b3a5ee2
Natan <mouranatan933@gmail.com>
2023-08-21 18:14:24 -0300

    feat: Reanimated, Gesture Handler

--- App.tsx
+++ App.tsx
@@ -3,7 +3,9 @@ import { Text, View } from "react-native";
 import Routes from "./src/routes";
 
 export default function App() {
-  return <>
-  <Routes/>
-  </>;
+  return (
+    <>
+      <Routes />
+    </>
+  );
 }
--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-  plugins: ["nativewind/babel"],
+   plugins: ["nativewind/babel", 'react-native-reanimated/plugin'],
+  
   };
 };
